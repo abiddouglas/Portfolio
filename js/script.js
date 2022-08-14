1a90b1487fa03dd2b782e2198e1e58de458481df
caspgin <asarangwala@popreach.com>
2022-08-14 04:23:56 -0700

    project updated

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function disableScroll() {
+    
+    window.onscroll = function() {
+        window.scrollTo(0 ,0);
+    };
+}
+
+function enable() {
+    window.onscroll = function() {};
+    const wrapper = document.getElementById('landing');
+    // wrapper.classList.add('rotateAway');
+    window.scrollTo(0 ,940);
+    wrapper.style.display = "none";
+}
+
+let enableScrolling = false;
+disableScroll()
+
+
+
+
+
+
